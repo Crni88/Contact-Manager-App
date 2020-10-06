2db3e1e6eedb2367f80ed52ff7f2a5e46cce4c7b
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2020-10-06 15:26:28 +0200

    Added Routing, and made seperate pages for add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from "react";
 import "./App.css";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "./context";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Contact from "./components/contacts/Contact";
+import About from "./components/Pages/About";
+import NotFound from "./components/Pages/NotFound";
 
 class App extends React.Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header />
-          <div className="container">
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts}></Route>
+                <Route exact path="/about" component={About}></Route>
+                <Route exact path="/contact/add" component={AddContact}></Route>
+                <Route component={NotFound}></Route>
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
--- src/context.js
+++ src/context.js
@@ -1,42 +1,40 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
-
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+    case "ADD_CONTACT":
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts],
+      };
+    default:
+      return state;
+  }
+};
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "kahric.tarik@gmail.com",
-        phone: "0123456789",
-      },
-      {
-        id: 2,
-        name: "Amna Kahric",
-        email: "kahric.amna@gmail.com",
-        phone: "987654321",
-      },
-      {
-        id: 3,
-        name: "Bilal Hodzic",
-        email: "hodzic.bilal@gmail.com",
-        phone: "225883",
-      },
-      {
-        id: 4,
-        name: "Emir Kurtovic",
-        email: "kurtovic.emir@gmail.com",
-        phone: "0644002619",
-      },
-      {
-        id: 5,
-        name: "Anes Kurtovic",
-        email: "kurtovic.anes@gmail.com",
-        phone: "0644004619",
-      },
-    ],
+    contacts: [],
+    dispatch: (action) => {
+      this.setState((state) => reducer(state, action));
+    },
   };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) =>
+      this.setState({
+        contacts: res.data,
+      })
+    );
+  }
   render() {
     return (
       <Context.Provider value={this.state}>
