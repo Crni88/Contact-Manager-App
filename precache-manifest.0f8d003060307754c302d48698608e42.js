3f84327e52b33e09fbd48b588572906643108729
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2020-10-19 13:56:25 +0200

    Updates

---
+++ precache-manifest.0f8d003060307754c302d48698608e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13441bd10a4cd6b3c5832581d52e81c0",
+    "url": "/Contact-Manager-App/index.html"
+  },
+  {
+    "revision": "dba5721ee4bf55854c80",
+    "url": "/Contact-Manager-App/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "de410388abf2b0002fd6",
+    "url": "/Contact-Manager-App/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "dba5721ee4bf55854c80",
+    "url": "/Contact-Manager-App/static/js/2.4af043e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager-App/static/js/2.4af043e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de410388abf2b0002fd6",
+    "url": "/Contact-Manager-App/static/js/main.595f86e3.chunk.js"
+  },
+  {
+    "revision": "0a438fd6b824040ff718",
+    "url": "/Contact-Manager-App/static/js/runtime-main.61c51320.js"
+  }
+]);
